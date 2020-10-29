93f7fd8dd11401545788b089e864005a31b2cec4
Burak YALCIN <burakyalcinpro@hotmail.com>
2020-10-29 17:25:55 +0100

    test
    Commit test

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import classes from './App.module.css';
+import ProductInfo from './ProductData';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className ="App-header">
+      <nav className={classes.topbar}>
+        <img src="https://i.dlpng.com/static/png/197684_preview.png" alt="Amazon Logo"/>
+      </nav>
       </header>
+
+      <div className={classes.Container}>
+        <div className={classes.ProductImage}>
+          <img src="https://imgur.com/iOeUBV7.png" alt="Product Image"/>
+        </div>
+        <div className={classes.ProductInfo}>
+          <h1 className= {classes.ProductTitle}>{ProductInfo.title}</h1>
+        </div>
+      </div>
     </div>
   );
 }
